f30aaf245e7fe3a10e4274fb88e879daf6b0b002
brunopj <brunopj1@hotmail.com>
2024-12-05 19:43:34 +0000

    2024 day 2

---
+++ Year2024/Days/Day02Solver.cs
@@ -0,0 +1,52 @@
+﻿using Common;
+
+namespace Year2024.Days;
+
+public class Day02Solver : Solver
+{
+    public override string SolveChallenge1(string[] input)
+    {
+        var parsed = ParseInput(input);
+        return parsed.Count(CheckLine).ToString();
+    }
+
+    public override string SolveChallenge2(string[] input)
+    {
+        var parsed = ParseInput(input);
+        return parsed.Count(x => CheckLine(x) || CheckLineButSkip(x)).ToString();
+    }
+
+    private static List<List<int>> ParseInput(string[] input)
+    {
+        return input.Select(x => x.Split(" ").Select(int.Parse).ToList()).ToList();
+    }
+
+    private static bool CheckLine(List<int> data)
+    {
+        var difs = new int[data.Count - 1];
+        for (var i = 0; i < data.Count - 1; i++)
+        {
+            difs[i] = data[i + 1] - data[i];
+        }
+
+        return
+            difs.All(x => x >= 1 && x <= 3) || // All incrementing
+            difs.All(x => x >= -3 && x <= -1); // All decrementing
+    }
+
+    private static bool CheckLineButSkip(List<int> data)
+    {
+        for (var i = 0; i < data.Count; i++)
+        {
+            var newData = new List<int>(data);
+            newData.RemoveAt(i);
+            
+            if (CheckLine(newData))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
