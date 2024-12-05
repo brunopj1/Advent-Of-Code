6a5764e332df03e01a5e8f8c1463c067325519d7
brunopj <brunopj1@hotmail.com>
2024-12-05 22:23:21 +0000

    2024 day 3

---
+++ Year2024/Days/Day03Solver.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.RegularExpressions;
+using Common;
+
+namespace Year2024.Days;
+
+public class Day03Solver : Solver
+{
+    public override string SolveChallenge1(string[] input)
+    {
+        var regex = new Regex(@"mul\((\d+),(\d+)\)");
+
+        var sum = 0;
+        
+        foreach (var line in input)
+        {
+            var results = regex.Matches(line);
+            
+            sum += results.Sum(x => int.Parse(x.Groups[1].Value) * int.Parse(x.Groups[2].Value));
+        }
+        
+        return sum.ToString();
+    }
+
+    public override string SolveChallenge2(string[] input)
+    {
+        var regex = new Regex(@"mul\((\d+),(\d+)\)|do\(\)|don't\(\)");
+
+        var doing = true;
+        var sum = 0;
+        
+        foreach (var line in input)
+        {
+            var results = regex.Matches(line);
+
+            foreach (Match? match in results)
+            {
+                if (match.Value == "do()")
+                {
+                    doing = true;
+                }
+                else if (match.Value == "don't()")
+                {
+                    doing = false;
+                }
+                else if (doing)
+                {
+                    sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+                }
+            }
+        }
+        
+        return sum.ToString();
+    }
+}
