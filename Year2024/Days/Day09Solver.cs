f75f7de0632a2a2e024c21b372a77ddeb652eaf7
brunopj <brunopj1@hotmail.com>
2024-12-15 17:53:22 +0000

    2024 day 9 part 2

--- Year2024/Days/Day09Solver.cs
+++ Year2024/Days/Day09Solver.cs
@@ -13,13 +13,13 @@ public class Day09Solver : Solver
             Count = count;
         }
         
-        public int Number { get; init; }
+        public int Number { get; set; }
         public int Count { get; set; }
     }
 
     public override object SolveChallenge1(string[] input)
     {
-        ParseInput(input, out var files, out var spaces);
+        ParseInput1(input, out var files, out var spaces);
 
         var filesIdx = 1;
 
@@ -67,10 +67,53 @@ public class Day09Solver : Solver
 
     public override object SolveChallenge2(string[] input)
     {
-        return "";
+        var files = ParseInput2(input);
+
+        for (var i = files.Count - 1; i >= 0; i--)
+        {
+            var file = files[i];
+            
+            if (file.Number < 0) continue;
+
+            for (var j = 0; j < i; j++)
+            {
+                var space = files[j];
+                
+                if (space.Number >= 0) continue;
+
+                if (space.Count == file.Count)
+                {
+                    space.Number = file.Number;
+                    file.Number = -1;
+                    break;
+                }
+                
+                if (space.Count > file.Count)
+                {
+                    files.Insert(j, new FileInfo(file.Number, file.Count));
+                    space.Count -= file.Count;
+                    file.Number = -1;
+                    i++;
+                    break;
+                }
+            }
+        }
+        
+        BigInteger result = 0;
+        var resultIdx = 0;
+
+        foreach (var file in files)
+        {
+            BigInteger from = resultIdx;
+            BigInteger to = resultIdx + file.Count - 1;
+            result += (to - from + 1) * (from + to) / 2 * int.Max(file.Number, 0);
+            resultIdx += file.Count;
+        }
+        
+        return result;
     }
 
-    private static void ParseInput(string[] input, out List<FileInfo> files, out List<int> spaces)
+    private static void ParseInput1(string[] input, out List<FileInfo> files, out List<int> spaces)
     {
         files = [];
         spaces = [];
@@ -82,5 +125,19 @@ public class Day09Solver : Solver
             if (isFile) files.Add( new FileInfo(i/2, count));
             else spaces.Add(count);
         }
+    }
+    
+    private static List<FileInfo> ParseInput2(string[] input)
+    {
+        var files = new List<FileInfo>();
+
+        for (var i = 0; i < input[0].Length; i++)
+        {
+            var count = input[0][i] - '0';
+            var isFile = i % 2 == 0;
+            files.Add( new FileInfo( isFile ? i / 2 : -1, count));
+        }
+        
+        return files;
     }
 }
